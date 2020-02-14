af94edbf890c0041a5fbb8419e31ae826e1c33e1
ilevelin <47749162+ilevelin@users.noreply.github.com>
2020-02-14 19:52:55 +0100

    14022020

---
+++ AVD/Assets/JoseManuelGonzalez/Scripts/Playground/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Player player;
+    public float runSpeed=0f, horizontalMove=0f;
+    public bool jump=false, crouch=false;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal") * runSpeed;
+
+        jump = Input.GetButtonDown("Jump");
+
+        if (Input.GetButtonDown("Crouch")) crouch = true;
+        if (Input.GetButtonUp("Crouch")) crouch = false;
+
+        Debug.Log("jump = " + jump + "// crouch = " + crouch);
+
+       // player.Move(horizontalMove * Time.deltaTime, crouch, jump);
+
+    }
+    void FixedUpdate()
+    {
+        player.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
+}
