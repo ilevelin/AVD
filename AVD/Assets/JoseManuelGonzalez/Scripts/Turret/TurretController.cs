49a3c542c070f1b01b2be8c26c2ef67ef226eb4e
ilevelin <47749162+ilevelin@users.noreply.github.com>
2020-03-06 19:36:21 +0100

    Que se me olvida kek

--- AVD/Assets/JoseManuelGonzalez/Scripts/Turret/TurretController.cs
+++ AVD/Assets/JoseManuelGonzalez/Scripts/Turret/TurretController.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class TurretController : MonoBehaviour
 {
 
+    /*
+      scripts resueltos:
+         https://github.com/amunyoz/AVD/blob/Turret/AVD/Assets/TriggerInputP.cs
+         https://github.com/amunyoz/AVD/blob/Turret/AVD/Assets/TurretActivator.cs
+         https://github.com/amunyoz/AVD/blob/Turret/AVD/Assets/FireBullets3d.cs
+     */
+
     void Start()
     {
         
